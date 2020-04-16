9748a0f37fbe7f1295cdc66ad252a66ff2b63c68
JC Izquierdo <jcizquierdo@Joses-Air.lan1>
2020-04-16 16:38:30 -0500

    added moment.js to the app

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,6 +13,8 @@ var createTask = function(taskText, taskDate, taskList) {
   // append span and p element to parent li
   taskLi.append(taskSpan, taskP);
 
+  //check due date
+  auditTask(taskLi);
 
   // append to ul list on the page
   $("#list-" + taskList).append(taskLi);
@@ -103,13 +105,22 @@ $(".list-group").on("click", "span", function() {
   // swap out elements
   $(this).replaceWith(dateInput);
 
+  //enable jquery ui datepicker
+  dateInput.datepicker({
+    minDate: 1,
+    onClose: function() {
+      // when calendar is closed, force a "change" event on the `dateInput`
+      $(this).trigger("change");
+    }
+  })
+
   // automatically focus on new element
   dateInput.trigger("focus");
 });
 
 
 // value of due date was changed
-$(".list-group").on("blur", "input[type='text']", function() {
+$(".list-group").on("change", "input[type='text']", function() {
   // get current text
   var date = $(this)
     .val()
@@ -137,6 +148,9 @@ $(".list-group").on("blur", "input[type='text']", function() {
 
   // replace input with span element
   $(this).replaceWith(taskSpan);
+
+  // Pass task's <li> element into auditTask() to check new due date
+  auditTask($(taskSpan).closest(".list-group-item"));
 });
 
 
@@ -188,7 +202,6 @@ $(".card .list-group").sortable({
   tasks[arrName] = tempArr;
   saveTasks();
   
-  console.log(tempArr);
   },
 })
 
@@ -197,17 +210,34 @@ $("#trash").droppable({
   accept: ".card .list-group-item",
   tolerance: "touch",
   drop: function(event, ui) {
-    console.log("drop");
     ui.draggable.remove();
   },
   over: function(event, ui) {
-    console.log("over");
   },
   out: function(event, ui) {
-    console.log("out");
   }
   
 });
+
+
+var auditTask = function(taskEl) {
+  // get date from task element
+  var date = $(taskEl).find("span").text().trim();
+
+  // convert to moment object at 5:00pm
+  var time = moment(date, "L").set("hour", 17);
+
+  // remove any old classes from element
+  $(taskEl).removeClass("list-group-item-warning list-group-item-danger");
+
+  // apply new class if task is near/over due date
+  if (moment().isAfter(time)) {
+    $(taskEl).addClass("list-group-item-danger");
+  }
+  else if (Math.abs(moment().diff(time, "days")) <= 2) {
+    $(taskEl).addClass("list-group-item-warning");
+  }
+};
 
 
 
@@ -251,6 +281,13 @@ $("#task-form-modal .btn-primary").click(function() {
     saveTasks();
   }
 });
+
+
+$("#modalDueDate").datepicker({
+  minDate: 1
+
+});
+
 
 
 // remove all tasks
